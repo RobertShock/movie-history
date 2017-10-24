cd2bdcc64227c20e790c85986a9d4f4f75ed1b88
Robbie Shock <rob.f.shock@gmail.com>
2017-10-23 21:00:53 -0500

    created google link signin.  closes out #17

--- dist/app.js
+++ dist/app.js
@@ -65,6 +65,7 @@ module.exports = {domString, clearDom};
 "use strict";
 
 const tmdb = require('./tmdb');
+const firebaseApi = require('./firebaseApi');
 
 const pressEnter = () => {
   $(document).keypress((e) => {
@@ -95,19 +96,43 @@ const myLinks = () => {
 	});
 };
 
-module.exports = {pressEnter, myLinks};
-},{"./tmdb":6}],4:[function(require,module,exports){
+const googleAuth = () => {
+	$('#googleButton').click((e) => {
+		firebaseApi.authenticateGoogle().then(() => {
+
+		}).catch ((err) => {
+			console.log("error in authenticateGoogle", err);
+		});
+	});
+};
+
+module.exports = {pressEnter, myLinks, googleAuth};
+},{"./firebaseApi":4,"./tmdb":6}],4:[function(require,module,exports){
 "use strict";
 
 let firebaseKey = "";
+let userUid = "";
 
 const setKey = (key) => {
 	firebaseKey = key;
 };
 
-module.exports = {setKey};
-},{}],5:[function(require,module,exports){
+//Firebase: GOOGLE - Use input credentials to authenticate user.
+  let authenticateGoogle = () => {
+    return new Promise((resolve, reject) => {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider)
+        .then((authData) => {
+        	userUid = authData.user.uid;
+            resolve(authData.user);
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+  };
 
+module.exports = {setKey, authenticateGoogle};
+},{}],5:[function(require,module,exports){
 "use strict";
 
 let events = require('./events');
@@ -115,6 +140,7 @@ let apiKeys = require('./apiKeys');
 
 apiKeys.retrieveKeys();
 events.myLinks();
+events.googleAuth();
 events.pressEnter();
 },{"./apiKeys":1,"./events":3}],6:[function(require,module,exports){
 "use strict";
